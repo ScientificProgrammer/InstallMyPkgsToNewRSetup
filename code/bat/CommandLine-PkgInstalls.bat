24df0491c18676e2862f6e53bf21085dee243e45
BombcatsKEM <kenneth.milgram@us.af.mil>
2023-03-20 13:07:08 -0500

    Install fewer packages at a time to prevent overloading 'pak'

--- code/bat/CommandLine-PkgInstalls.bat
+++ code/bat/CommandLine-PkgInstalls.bat
@@ -2,6 +2,18 @@ RScript -e "if (!require(pak)) { try(install.packages('pak', repos = 'https://r-
 
 RScript -e "if (!require(glue)) { try(install.packages('glue', type = 'source', repo = 'https://cloud.r-project.org/', build_manual = TRUE, build_vignettes = TRUE)); sessioninfo::session_info(); } else cat('\n\n', 'package already exists...skipping install\n\n')"
 
-RScript -e "try(pak::pkg_install(c('r-lib/rlang', 'r-lib/vctrs', 'r-lib/pillar', 'r-lib/processx', 'r-lib/pkgload', 'r-lib/remotes', 'tidyverse/tibble'), upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
+RScript -e "try(pak::pkg_install('r-lib/rlang'     , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
+
+RScript -e "try(pak::pkg_install('r-lib/remotes'   , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
+
+RScript -e "try(pak::pkg_install('tidyverse/tibble', upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
+
+RScript -e "try(pak::pkg_install('r-lib/pillar'    , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
+
+RScript -e "try(pak::pkg_install('r-lib/processx'  , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
+
+RScript -e "try(pak::pkg_install('r-lib/pkgload'   , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
+
+RScript -e "try(pak::pkg_install('r-lib/vctrs'     , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
 
 REM RScript -e "try(remotes::install_github('tidyverse/tibble', build_vignettes = TRUE, build_manual = TRUE, dependencies = TRUE)); sessioninfo::session_info();"
