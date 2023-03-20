4c669dd6af968974ad9846c38e9c3bbd1ff095e5
BombcatsKEM <kenneth.milgram@us.af.mil>
2023-03-20 10:57:40 -0500

    Fix code to check for 'glue' pkg before installing it

--- code/bat/CommandLine-PkgInstalls.bat
+++ code/bat/CommandLine-PkgInstalls.bat
@@ -1,6 +1,6 @@
 RScript -e "if (!require(pak)) { try(install.packages('pak', repos = 'https://r-lib.github.io/p/pak/devel/', build_manual = TRUE, build_vignettes = TRUE, dependencies = TRUE)); sessioninfo::session_info(); } else cat('\n\n', 'package already exists...skipping install\n\n')"
 
-RScript -e "if (!require(pak)) { try(install.packages('glue', type = 'source', repo = 'https://cloud.r-project.org/', build_manual = TRUE, build_vignettes = TRUE)); sessioninfo::session_info(); } else cat('\n\n', 'package already exists...skipping install\n\n')"
+RScript -e "if (!require(glue)) { try(install.packages('glue', type = 'source', repo = 'https://cloud.r-project.org/', build_manual = TRUE, build_vignettes = TRUE)); sessioninfo::session_info(); } else cat('\n\n', 'package already exists...skipping install\n\n')"
 
 RScript -e "try(pak::pkg_install(c('r-lib/rlang', 'r-lib/vctrs', 'r-lib/pillar', 'r-lib/processx', 'r-lib/pkgload', 'r-lib/remotes', 'tidyverse/tibble'), upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
 
