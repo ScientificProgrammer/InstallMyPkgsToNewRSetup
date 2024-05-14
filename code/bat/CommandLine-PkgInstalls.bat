2655c1fedec1bc5ca340149f23bb4b7677c9ef78
kenneth.milgram <kenneth.milgram@us.af.mil>
2024-05-14 18:26:18 -0500

    Change `RScript` to `Rscript` to run on Linux (or other case sensitive OSs)

--- code/bat/CommandLine-PkgInstalls.bat
+++ code/bat/CommandLine-PkgInstalls.bat
@@ -1,19 +1,19 @@
-RScript -e "if (!require(pak)) { try(install.packages('pak', repos = 'https://r-lib.github.io/p/pak/devel/', build_manual = TRUE, build_vignettes = TRUE, dependencies = TRUE)); sessioninfo::session_info(); } else cat('\n\n', 'package already exists...skipping install\n\n')"
+Rscript -e "if (!require(pak)) { try(install.packages('pak', repos = 'https://r-lib.github.io/p/pak/devel/', build_manual = TRUE, build_vignettes = TRUE, dependencies = TRUE)); sessioninfo::session_info(); } else cat('\n\n', 'package already exists...skipping install\n\n')"
 
-RScript -e "if (!require(glue)) { try(install.packages('glue', type = 'source', repo = 'https://cloud.r-project.org/', build_manual = TRUE, build_vignettes = TRUE)); sessioninfo::session_info(); } else cat('\n\n', 'package already exists...skipping install\n\n')"
+Rscript -e "if (!require(glue)) { try(install.packages('glue', type = 'source', repo = 'https://cloud.r-project.org/', build_manual = TRUE, build_vignettes = TRUE)); sessioninfo::session_info(); } else cat('\n\n', 'package already exists...skipping install\n\n')"
 
-RScript -e "try(pak::pkg_install('r-lib/rlang'     , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
+Rscript -e "try(pak::pkg_install('r-lib/rlang'     , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
 
-RScript -e "try(pak::pkg_install('r-lib/remotes'   , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
+Rscript -e "try(pak::pkg_install('r-lib/remotes'   , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
 
-RScript -e "try(pak::pkg_install('tidyverse/tibble', upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
+Rscript -e "try(pak::pkg_install('tidyverse/tibble', upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
 
-RScript -e "try(pak::pkg_install('r-lib/pillar'    , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
+Rscript -e "try(pak::pkg_install('r-lib/pillar'    , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
 
-RScript -e "try(pak::pkg_install('r-lib/processx'  , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
+Rscript -e "try(pak::pkg_install('r-lib/processx'  , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
 
-RScript -e "try(pak::pkg_install('r-lib/pkgload'   , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
+Rscript -e "try(pak::pkg_install('r-lib/pkgload'   , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
 
-RScript -e "try(pak::pkg_install('r-lib/vctrs'     , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
+Rscript -e "try(pak::pkg_install('r-lib/vctrs'     , upgrade = TRUE, dependencies = pkgdepends::as_pkg_dependencies('all'))); sessioninfo::session_info()"
 
-REM RScript -e "try(remotes::install_github('tidyverse/tibble', build_vignettes = TRUE, build_manual = TRUE, dependencies = TRUE)); sessioninfo::session_info();"
+REM Rscript -e "try(remotes::install_github('tidyverse/tibble', build_vignettes = TRUE, build_manual = TRUE, dependencies = TRUE)); sessioninfo::session_info();"
